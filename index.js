ffd5f11dd40715100352866380ad9608483e2334
Jason Kramer <jasonkramer525@gmail.com>
2020-06-27 18:07:19 -0700

    Added 5 working sort functions

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var totalResults = 496;
 var currentPage = 0;
 var pageNumber = 1;
 var load = 1; //video
+var chosenSort = 0;
 
 function changeColor(name) {
 	let button = document.getElementById(name);
@@ -14,6 +15,27 @@ function changeColor(name) {
 		button.setAttribute("class","btn btn-danger btn-sm");
 	}
 }
+
+function setSort(val){
+	if(val == chosenSort)
+		return;
+	chosenSort = val;
+
+	document.getElementById('sort-val').innerHTML = sortRequirements[chosenSort].name
+
+	for(let i=0;i<17;i++){
+		let button = document.getElementById('sort-'+i)
+		if(i == val){
+			button.setAttribute("class","btn btn-success btn-sm");
+		}
+		else {
+			button.setAttribute("class","btn btn-danger btn-sm");
+		}
+	}
+
+	readData();
+}
+
 
 function setPageSize(size){
 	currentPage = 0;
@@ -100,47 +122,64 @@ function readData()
 {
 	var tableBody = document.getElementById("table-body");
 	tableBody.innerHTML ="";
-	var currentSort = videoID.slice(0) //clone the array
 
-	let indeces = sortWithIndeces(currentSort).sortIndices;
+	var currentSort = sortRequirements[chosenSort].array.slice(0) //clone the array
 
-	//videoTitle = updateArray(videoTitle,indeces)
-	//thumbnailID = updateArray(thumbnailID,indeces)
+	var indeces;
+
+	if(sortRequirements[chosenSort].value == "integer") {
+		indeces = sortWithIndecesValues(currentSort).sortIndices;
+	}
+	else{
+		indeces = sortWithIndeces(currentSort).sortIndices;
+	}
+
+	if(sortRequirements[chosenSort].type == "reverse") {
+		indeces = indeces.reverse();
+	}
 
 	let currentCount = currentPage;
 	for(ID in videoID){
 		currentCount = currentCount + 1;
 		if(videoID[currentCount-1]) {
-		let row = tableBody.insertRow();
-		row.className = "d-flex"
-		let countCell = row.insertCell(0);
-		countCell.className = "col-1"
-		let text = document.createTextNode(currentCount);
-		countCell.appendChild(text);
+			let row = tableBody.insertRow();
+			row.className = "d-flex"
+			let countCell = row.insertCell(0);
+			countCell.className = "col-1"
+			let text = document.createTextNode(currentCount);
+			countCell.appendChild(text);
 
-		let videoCell = row.insertCell(1);
-		videoCell.className = "col-5"
-		let div = document.createElement('div');
+			let videoCell = row.insertCell(1);
+			videoCell.className = "col-5"
+			let div = document.createElement('div');
 
-		if(load == 1){
-			div.className = "videoWrapper";
-			div.innerHTML = "<iframe width='560' height='315' src='https://www.youtube.com/embed/" + videoID[indeces[currentCount-1]] + "'frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
-		} else if(load == 2){
-			div.innerHTML = '<a href=https://www.youtube.com/watch?v=' + videoID[indeces[currentCount-1]] + '" target="_blank" rel="noopener noreferrer" >' + "<img style='width:100%; display:block;' src='" + thumbnailID[indeces[currentCount-1]] + "'></a>"
-		}
-		else
-			div.innerHTML = '<a href=https://www.youtube.com/watch?v=' + videoID[indeces[currentCount-1]] + '" target="_blank" rel="noopener noreferrer" >youtube.com/watch?v=' + videoID[indeces[currentCount-1]] + '</a>'
-		
-		videoCell.appendChild(div)
+			if(load == 1){
+				div.className = "videoWrapper";
+				div.innerHTML = "<iframe width='560' height='315' src='https://www.youtube.com/embed/" + videoID[indeces[currentCount-1]] + "'frameborder='0' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture' allowfullscreen></iframe>"
+			} else if(load == 2){
+				div.innerHTML = '<a href=https://www.youtube.com/watch?v=' + videoID[indeces[currentCount-1]] + '" target="_blank" rel="noopener noreferrer" >' + "<img style='width:100%; display:block;' src='" + thumbnailID[indeces[currentCount-1]] + "'></a>"
+			}
+			else
+				div.innerHTML = '<a href=https://www.youtube.com/watch?v=' + videoID[indeces[currentCount-1]] + '" target="_blank" rel="noopener noreferrer" >youtube.com/watch?v=' + videoID[indeces[currentCount-1]] + '</a>'
+			
+			videoCell.appendChild(div)
 
-		let titleCell = row.insertCell(2)
-		titleCell.className = "col-3"
-		text = document.createTextNode(videoTitle[indeces[currentCount-1]]);
-		titleCell.append(text)
+			let titleCell = row.insertCell(2)
+			titleCell.className = "col-3"
+			text = document.createTextNode(videoTitle[indeces[currentCount-1]]);
+			titleCell.append(text)
 
+			let sortCell = row.insertCell(3)
+			sortCell.className = 'col-3'
+			var sortText;
+			if(sortRequirements[chosenSort].number=="true"){
+				sortText = document.createTextNode(numberWithCommas(viewCount[[indeces[currentCount-1]]]))
+			}
+			else {
+				sortText = document.createTextNode(viewCount[[indeces[currentCount-1]]])
+			}
 
-		console.log(videoID[ID])
-
+			sortCell.append(sortText)
 		}
 
 		if(ID>pageTotal-2){
@@ -165,10 +204,27 @@ function sortWithIndeces(toSort) {
   return toSort;
 }
 
+function sortWithIndecesValues(toSort) {
+  for (var i = 0; i < toSort.length; i++) {
+    toSort[i] = [toSort[i], i];
+  }
+  toSort.sort(function (a,b) { return a[0]-b[0]; });
+  toSort.sortIndices = [];
+  for (var j = 0; j < toSort.length; j++) {
+    toSort.sortIndices.push(toSort[j][1]);
+    toSort[j] = toSort[j][0];
+  }
+  return toSort;
+}
+
 function updateArray(toSort,idxArray){
 	let newArray = new Array(idxArray.length)
 	for(idx in toSort){
 		newArray[idx]=toSort[idxArray[idx]]
 	}
 	return newArray
+}
+
+function numberWithCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
